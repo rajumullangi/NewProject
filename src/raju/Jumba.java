8a1e890698f12b241f606fe10086c869ec87d1b4
AdmiN <AdmiN@ITWS0047>
2024-08-27 10:18:14 +0530

    dgdg

--- src/raju/Jumba.java
+++ src/raju/Jumba.java
@@ -1,5 +1,18 @@
 package raju;
+import java.util.Scanner;
+public class Login {
+ public static void main(String[] args) {
+     try (Scanner scanner = new Scanner(System.in)) {
+         System.out.print("Enter user name => ");
+         String userName = scanner.nextLine();
+         System.out.print("Enter password => ");
+         String password = scanner.nextLine();
 
-public class Jumba {
-
+         if ("Jumba".equals(userName) && "123Jumba".equals(password)) {
+             System.out.println("User successfully logged-in.");
+         } else {
+             System.out.println("Invalid username or password.");
+         }
+     }
+ }
 }
